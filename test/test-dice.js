59ecf179a2a38e05a85fa34fe3064b648212fa2e
Jerry Hamlet <jerry@hamletink.com>
2012-03-30 07:31:42 -0700

    Basic die rolling and basic behaviors. TODO: Break behaviors into individual objects.

--- test/test-dice.js
+++ test/test-dice.js
@@ -1,36 +1,144 @@
 
-var Dice = require("../").Dice;
+var Dice = require("../").Dice,
+    diceBehaviors = require("../").behaviors;
 
 suite("Dice", function () {
     
     test("Instantiation", function () {
         var dice = new Dice(3, 6);
         
-        dice.dice.length.should.equal(3);
+        dice.count.should.equal(3);
         dice.rolled.should.equal(false);
-        dice.result.should.equal(0);
+        dice.total.should.equal(0);
+        ("" + dice).should.equal("3d6");
+    });
+    
+    test("Roll d6", function () {
+        var dice = new Dice(1, 6),
+            result
+        ;
+
+        ("" + dice).should.equal("d6");
+        
+        result = dice.roll();
+        result.should.be.within(1, 6);
+        dice.rolled.should.equal(true);
+        dice.total.should.not.equal(0);
+        dice.dice.length.should.equal(1);
+        
+        console.log("" + dice);
+        ("" + dice).should.match(/d6 \[(\d+)\] = \d+/);
     });
 
-    test("Handful of the same type of dice", function () {
+    test("Roll 3d6", function () {
         var dice = new Dice(3, 6),
             result
         ;
         
+        ("" + dice).should.equal("3d6");
+
         result = dice.roll();
         result.should.be.within(1, 18);
         dice.rolled.should.equal(true);
-        dice.result.should.not.equal(0);
+        dice.total.should.not.equal(0);
+        dice.dice.length.should.equal(3);
+        
+        console.log("" + dice);
+        ("" + dice).should.match(/d6 \[(\d+(?:, )?){3}\] = \d+/);
     });
     
-    test("Instantiation with a string", function () {
-        var dice = new Dice("3d6"),
-            result
+    test("Roll 3d6+2", function () {
+        var dice = new Dice(3, 6, [diceBehaviors.resultModifier(2)]),
+            result, dietotal
         ;
         
         result = dice.roll();
-        // result.should.be.within(1, 18);
-        dice.rolled.should.equal(true);
-        dice.result.should.not.equal(0);
+        result.should.be.within(5, 20);
+        
+        dietotal = dice.dice.reduce(function (p, d) {
+            return p + d;
+        }, 0);
+        
+        dietotal.should.equal(result - 2);
+
+        console.log("" + dice);
+    });
+
+    test("Roll 3d6 and add 1 to every die", function () {
+        var dice = new Dice(3, 6, [diceBehaviors.dieModifier(1)]),
+            result, dietotal
+        ;
+        
+        result = dice.roll();
+        result.should.be.within(6, 21);
+        
+        dietotal = dice.dice.reduce(function (p, d) {
+            return (p + d);
+        }, 0);
+        
+        dietotal.should.equal(result - 3);
+
+        console.log("" + dice);
+    });
+    
+    test("5d6 minus two lowest (5d6-2low)", function () {
+        var dice = new Dice(5, 6),
+            results
+        ;
+        
+        dice.addBehavior(diceBehaviors.removeLowest(2));
+        
+        results = dice.roll();
+        results.should.be.within(3, 18);
+        
+        dice.dice.length.should.equal(3);
+        dice.lowDice.length.should.equal(2);
+        
+        console.log("" + dice);
+    });
+    
+    test("10d6 Hero Dice", function () {
+        var dice = new Dice(10, 6);
+        
+        dice.addBehavior(diceBehaviors.heroDice());
+        
+        dice.roll();
+        dice.stun.should.be.within(10, 60);
+        dice.body.should.be.within(0, 20);
+        
+        console.log("" + dice);
+    });
+    
+    test("6d6 Success dice", function () {
+        var dice = new Dice(6, 6);
+        
+        dice.addBehavior(diceBehaviors.successDice(5));
+        
+        dice.roll();
+        dice.total.should.be.within(0, 6);
+
+        console.log("" + dice);
+    });
+    
+    test("6d6 Shadowrun dice", function () {
+        var dice = new Dice(6, 6);
+        
+        dice.addBehavior(
+            diceBehaviors.wildDice(),
+            diceBehaviors.successDice(5)
+        );
+        
+        dice.roll();
+        
+        console.log("" + dice);
+    });
+    
+    test("6d6 WEG Star Wars", function () {
+        var dice = new Dice(6, 6);
+        
+        dice.addBehavior(diceBehaviors.wildDice(1));
+        
+        dice.roll();
         
         console.log("" + dice);
     });
