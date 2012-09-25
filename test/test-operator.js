a03f7e653db934eb3e594eab7cddbff91a6e2e41
Jerry Hamlet <jerry@hamletink.com>
2012-09-25 06:55:13 -0700

    More robust operator test.

--- test/test-operator.js
+++ test/test-operator.js
@@ -1,60 +1,68 @@
 /*globals suite, test, setup, teardown */
 
-var Dice       = require("dice-js"),
-    Operator   = Dice.Operator,
-    Expression = Dice.Expression,
-    should     = require("should")
+var Dice        = require("dice-js"),
+    should      = require("should"),
+    repeat      = require("./repeat"),
+    Operator    = Dice.Operator,
+    Expression  = Dice.Expression,
+    format      = require("util").format,
+    tries       = 5000,
+    faces       = [4, 6, 8, 10, 12, 20, 100],
+    ops = [
+        {
+            name: "Addition",
+            symbol: "+"
+        },
+        {
+            name: "Subtraction",
+            symbol: "-"
+        },
+        {
+            name: "Division",
+            symbol: "/"
+        },
+        {
+            name: "Multiplication",
+            symbol: "*"
+        }
+    ]
 ;
+
+function evalMath (left, operator, right) {
+    return eval(format("%s %s %s", left, operator, right))
+}
 
 suite("Operator", function () {
     
-    test("Very basic addition", function () {
-        var op          = new Dice.Operator("+", 8, 5),
-            PlusOp      = Dice.Operator.Addition,
-            Ability     = Dice.FfgStarWarsAbility,
-            Proficiency = Dice.FfgStarWarsProficiency,
-            Difficulty  = Dice.FfgStarWarsDifficulty,
-            Challenge   = Dice.FfgStarWarsChallenge,
-            Boost       = Dice.FfgStarWarsBoost,
-            Setback     = Dice.FfgStarWarsSetback,
-            Force       = Dice.FfgStarWarsForce
+    ops.forEach(function (obj) {
+        var left    = 8,
+            right   = 5,
+            symbol  = obj.symbol,
+            result  = evalMath(left, symbol, right)
         ;
-        // op.valueOf().should.equal(13);
         
-        op = new Dice.Operator.Addition(new Dice(3, 6), 12);
-        op.valueOf().should.be.within(15, 30);
-        console.log(op.valueOf());
+        test(format("Numeric %s", obj.name), function () {
+            var op = new Operator[symbol](left, right);
+            op.valueOf().should.equal(result);
+        });
+
+        faces.forEach(function (faces) {
+
+            test(format("D%s %s (%s times)", faces, obj.name, tries), function () {
+
+                repeat(tries, function (nil, idx) {
+                    var left    = new Dice(faces),
+                        right   = new Dice(faces),
+                        op      = new Operator[symbol](left, right),
+                        result  = evalMath(left.result, symbol, right.result)
+                    ;
+
+                    op.valueOf().should.equal(result);
+                });
+            });
+
+        });
         
-        op = new Dice.Operator["+"](new Dice(8, 6), new Dice(8, 6));
-        op.valueOf().should.be.within(16, 96);
-        // console.log(op.valueOf());
-        
-        op = new PlusOp(Force(4), new PlusOp(Force(1), Setback(1)));
-        console.log(op.valueOf());
-        
-        op = new PlusOp(
-            Ability(2),
-            new PlusOp(
-                Proficiency(1),
-                new PlusOp(
-                    Difficulty(1),
-                    new PlusOp(
-                        Challenge(1),
-                        new PlusOp(
-                            Boost(1),
-                            Setback(1)
-                        )
-                    )
-                )
-            )
-        );
-        console.log(op.valueOf());
-        
-        op = new PlusOp(
-            new Expression(new Dice(3, 6), new Dice(3, 6), new Dice(3, 6)),
-            new Expression(new Dice(2, 8), new Dice(2, 8), new Dice(2, 8))
-        );
-        console.log(op.valueOf());
     });
     
 });
